460192b5e41c3a027cd99feb1af6e925e939831f
anxiety16 <jhonnymahinay3@gmail.com>
2024-11-06 00:36:42 +0800

    view.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
+from typing import Any
+from django.db.models.query import QuerySet
+from django.db.models import Q
 
 class HomePageView(ListView):
     model = Organization
@@ -16,6 +19,13 @@ class OrganizationList(ListView):
     template_name = 'org_list.html'
     paginate_by = 5
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrganizationList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get("q") is not None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query))
+        return qs
+
 class OrganizationCreateView(CreateView):
     model = Organization
     form_class = OrganizationForm
@@ -27,6 +37,12 @@ class OrganizationUpdateView(UpdateView):
     form_class = OrganizationForm
     template_name = 'org_edit.html'
     success_url = reverse_lazy('organization-list')
+
+class OrganizationDeleteView(DeleteView):
+    model = Organization
+    template_name = 'org_del.html'
+    success_url = reverse_lazy('organization-list')
+
 
 
 
